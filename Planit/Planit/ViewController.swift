97055174b454f8bd0b266525725a77526c2a8448
Jiye Choi <jiyechoi@umass.edu>
2020-10-14 02:10:33 -0400

    added main page

--- Planit/Planit/ViewController.swift
+++ Planit/Planit/ViewController.swift
@@ -8,15 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource{
     
+    
+    @IBOutlet weak var addButton: UIButton!
+    
+    @IBOutlet weak var taskTable: UITableView!
+    let taskArray = ["490P Demo", "453 Midtem", "590G HW4"]
+    let cellID = "cellID"
+      
     override func viewDidLoad() {
         super.viewDidLoad()
+        taskTable.delegate = self
+        taskTable.dataSource = self
     }
 
     // to identify the segue named "firstLink" (main - calendar) and follow that segue to open another view.
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         performSegue(withIdentifier: "firstLink", sender: self)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // To define how many rows are needed
+        return taskArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // reuns whenever the table view needs to put data in its rows
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
+        if(cell == nil) {
+            cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: cellID)
+        }
+        cell?.textLabel?.text = taskArray[indexPath.row]
+        return cell!
     }
 }
 
