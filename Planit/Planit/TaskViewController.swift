dc91086c314d0a6d45c3dd33004700411936faa9
Jiye Choi <jiyechoi@umass.edu>
2020-10-14 01:35:20 -0400

    added task page

---
+++ Planit/Planit/TaskViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TaskViewController.swift
+//  Planit
+//
+//  Created by Jiye Choi on 10/14/20.
+//  Copyright © 2020 Pete Walker. All rights reserved.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    @IBOutlet weak var addButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
